c3324d0129d59e86070bb2687c146dcb78186e01
liuky <liuky@ibe.com>
2024-10-25 14:29:08 +0800

    修改 curl_cffi 版本

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         'request_manager': ['*.js'],
     },
     install_requires=[
-        'curl_cffi==v0.6.4',
+        'curl_cffi==0.7.3',
         'schedule==1.2.2',
         'PyExecJS==1.5.1'
     ],
